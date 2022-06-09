2063b96afa8443e028ec39f3ffa3acf2325f4e48
suchitrathapa <83858230+suchitrathapa@users.noreply.github.com>
2022-06-09 21:57:55 +0545

    Update Stage_One_Task.sh

--- Stage_One_Task.sh
+++ Stage_One_Task.sh
@@ -31,17 +31,35 @@ conda install -c bioconda spades
 # uploading forward and reverse read dataset from github link
 wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/Alsen_R1.fastq.gz?raw=true -O Alsen_R1.fastq.gz
 wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/Alsen_R2.fastq.gz?raw=true -O Alsen_R2.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/ACBarrie_R1.fastq.gz?raw=true -O ACBarrie_R1.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/ACBarrie_R2.fastq.gz?raw=true -O ACBarrie_R2.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/Chara_R1.fastq.gz?raw=true -O Chara_R1.fastq.gz
+wget https://github.com/josoga2/yt-dataset/blob/main/dataset/raw_reads/Chara_R2.fastq.gz?raw=true -O Chara_R2.fastq.gz
 
 # creating an output folder to send analysis output into it
 mkdir output
 
 # running fastqc on reads that was downloaded earlier and sending the report file into “output” folder
-fastqc Alsen_R1.fastq.gz -o output/
-fastqc Alsen_R2.fastq.gz -o output/
+fastqc *.fastq.gz -O output/
 
-# running fastp on reads that was downloaded earlier and saving the output as new fastp file 
-fastp -i Alsen_R1.fastq.gz -o Alsen_R1.fastp.gz
-fastp -i Alsen_R2.fastq.gz -o Alsen_R2.fastp.gz
+# running fastp on reads that was downloaded earlier and saving the output as html file 
+SAMPLES=(
+"Alsen"
+"ACBarrie"
+"Chara"
+)
+
+for SAMPLE in "${SAMPLES[@]}"; do
+
+fastp \
+        -i "$PWD/${SAMPLE)_R1.fastq.gz" \
+        -I "$PWD/${SAMPLE)_R2.fastq.gz" \
+        -o "output/${SAMPLE}_R1.fastq.gz" \
+        -O "output/${SAMPLE}_R2.fastq.gz" \
+        --html "output/${SAMPLE}_fastp.html"
+done
 
 # running spades on reads that was downloaded earlier and sending the report file into “spades_report” folder 
 spades.py -1 Alsen_R1.fastq.gz -2 Alsen_R2.fastq.gz --careful --cov-cutoff auto -o spades_report
+spades.py -1 ACBarrie_R1.fastq.gz -2 ACBarrie_R2.fastq.gz --careful --cov-cutoff auto -o spades_report
+spades.py -1 Chara_R1.fastq.gz -2 Chara_R2.fastq.gz --careful --cov-cutoff auto -o spades_report
