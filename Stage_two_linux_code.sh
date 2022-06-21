61b835652f739bb147ad2cf08975a3d81a594173
suchitrathapa <83858230+suchitrathapa@users.noreply.github.com>
2022-06-21 21:39:25 +0545

    Update Stage_two_linux_code.sh

--- Stage_two_linux_code.sh
+++ Stage_two_linux_code.sh
@@ -96,9 +96,13 @@ do
 samtools view -q 1 -f 0x2 -F 0x8 -b Mapping/${sample}.sorted.bam > Mapping/${sample}.filtered1.bam
 done
         
+#viewing the bam files
+
 samtools flagstat SLGFSK-T_231336.filtered1.bam
 
 samtools flagstat SLGFSK-N_231335.filtered1.bam
+
+# sorting and filtering bam file for checking duplicates using markdup
 
 for sample in `cat list.txt`
 do
@@ -108,30 +112,38 @@ samtools sort -@ 32 -o Mapping/${sample}.positionsort.bam Mapping/${sample}.fixm
 samtools markdup -@32 -r Mapping/${sample}.positionsort.bam Mapping/${sample}.clean.bam
 done
 
+# removing duplicates
+
 for sample in `cat list.txt`
 do
 samtools rmdup Mapping/${sample}.sorted.bam Mapping/${sample}.rdup
 done
 
+#
 for sample in `cat list.txt`
 do      
 cat Mapping/${sample}.clean.bam  | bamleftalign -f hg19.chr5_12_17.fa -m 5 -c > Mapping/${sample}.leftAlign.bam
 done
 
-  #Recalibrate read mapping qualities
+#Rechecking read mapping qualities
 
 for sample in `cat list.txt`
 do
 samtools calmd -@ 32 -b hg19.chr5_12_17.fa Mapping/${sample}.leftAlign.bam > Mapping/${sample}.recalibrate.bam
 done
 
+# Refiltering the reads
+
 for sample in `cat list.txt`
 do
 bamtools filter -in Mapping${sample}.recalibrate.bam -mapQuality "<=254" > Mapping/${sample}.refilter.bam
 done
 
+#Downloading the variant file
+
 wget https://sourceforge.net/projects/varscan/files/VarScan.v2.3.9.jar	   
         
+# making a pile up file for variant calling
 mkdir Variants       
         
 for sample in `cat list.txt`
@@ -139,12 +151,16 @@ do
 samtools mpileup -f hg19.chr5_12_17.fa Mapping/${sample}.refilter.bam --min-MQ 1 --min-BQ 28 \ > Variants/${sample}.pileup
 done
 
+# variant calling
+
 java -jar VarScan.v2.3.9.jar somatic Variants/SLGFSK-N_231335.pileup \
 
         Variants/SLGFSK-T_231336.pileup Variants/SLGFSK \
 
         --normal-purity 1  --tumor-purity 0.5 --output-vcf 1 
 
+
+# creating compressed files and indexing them for merging them at last using bcftools
 
 bgzip Variants/SLGFSK.snp.vcf > Variants/SLGFSK.snp.vcf.gz
 
@@ -156,5 +172,14 @@ tabix Variants/SLGFSK.indel.vcf.gz
 
 bcftools merge Variants/SLGFSK.snp.vcf.gz Variants/SLGFSK.indel.vcf.gz > Variants/SLGFSK.vcf
 
+# creating a database using snpEff
+
 wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
+
 unzip snpEff_latest_core.zip
+
+# annoting the vcf file
+
+java -Xmx8g -jar snpEff/snpEff.jar hg19 Variants/SLGFSK.vcf > Variants/SLGFSK.ann.vcf
+
+
